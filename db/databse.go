7651b2cc4c144abfc8237da4786087cd1cc111e7
mukti wibowo <muktiw7@gmail.com>
2024-12-24 21:39:50 +0700

    Added lesson for package import

---
+++ db/databse.go
@@ -0,0 +1,12 @@
+package db
+
+import "fmt"
+
+func init() {
+	fmt.Println("Connecting to db...")
+	fmt.Println("Connected to db!")
+}
+
+func PrintDb() {
+	fmt.Println("Hello form DB!")
+}
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	_ "crash-course/db"
+	"crash-course/helper"
+)
 
+func main() {
+	helper.SayHello()
 }
